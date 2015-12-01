0db6159b28a69f420d9ba47e6a06ddbf799ae392
svlugovoy <svlugovoy@gmail.com>
2015-12-01 15:03:53 +0200

    question 126, 131

--- src/ua/com/svlugovoy/youtube300plus/q10q11q12/Ternary.java
+++ src/ua/com/svlugovoy/youtube300plus/q10q11q12/Ternary.java
@@ -1,25 +1,21 @@
 package ua.com.svlugovoy.youtube300plus.q10q11q12;
 
-/**
- * Created by Sergey on 27.10.2015.
- */
 public class Ternary {
     public static void main(String args[]) {
         int number;
         String text;
 
-        number = 10;
+        number = 3;
 
         text = (number % 2 == 0) ? "четное" : "нечетное";
         System.out.println("Число " + number + " - " + text + "!");
 
-//        if (number % 2 == 0) {
-//            text = "четное";
-//        } else {
-//            text = "нечетное";
-//        }
-//        System.out.println("То же самое, только с if-else");
-//        System.out.println("Число " + number + " - " + text + "!");
-
+        if (number % 2 == 0) {
+            text = "четное";
+        } else {
+            text = "нечетное";
+        }
+        System.out.println("То же самое, только с if-else");
+        System.out.println("Число " + number + " - " + text + "!");
     }
 }
