8637bfa2dd283aea027a369a9063a90785c42420
svlugovoy <svlugovoy@gmail.com>
2016-02-18 12:12:45 +0200

    access to private member

---
+++ src/com/svlugovoy/youtube300plus/q80/DemoClass.java
@@ -0,0 +1,14 @@
+package com.svlugovoy.youtube300plus.q80;
+
+/**
+ * @author Sergey Lugovoy <svlugovoy@gmail.com> 18.02.2016.
+ */
+public class DemoClass {
+    public static void main(String[] args) {
+
+        TestClass test = new TestClass();
+        System.out.println(test.i_publ);
+//        System.out.println(test.i_priv);
+//        System.out.println(test.str_priv);
+    }
+}
