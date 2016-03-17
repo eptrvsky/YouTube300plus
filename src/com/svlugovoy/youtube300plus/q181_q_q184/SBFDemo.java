2b9a03680901d4bcb4118b51c652beed004995af
svlugovoy <svlugovoy@gmail.com>
2016-03-17 23:49:05 +0200

    String + SBF + SBD

---
+++ src/com/svlugovoy/youtube300plus/q181_q_q184/SBFDemo.java
@@ -0,0 +1,33 @@
+package com.svlugovoy.youtube300plus.q181_q_q184;
+
+/**
+ * @author Sergey Lugovoy <svlugovoy@gmail.com> 17.03.2016.
+ */
+public class SBFDemo {
+    public static void main(String[] args) {
+
+        StringBuffer sbf = new StringBuffer();
+        System.out.println("Длина - " + sbf.length());
+        System.out.println("Размер - " + sbf.capacity());
+        sbf = new StringBuffer(20);
+        sbf.append("Java");
+        System.out.println(sbf);
+        System.out.println("Длина - " + sbf.length());
+        System.out.println("Размер - " + sbf.capacity());
+        sbf.append(" is a programming language!");
+        System.out.println(sbf);
+        System.out.println("Длина - " + sbf.length());
+        System.out.println("Размер - " + sbf.capacity());
+        System.out.println("-------------------");
+
+        System.out.println(sbf);
+        StringBuffer sbf1 = new StringBuffer(48);
+        sbf1.append("Java is a programming language!");
+        System.out.println(sbf1);
+
+        System.out.println(sbf.equals(sbf1));
+        System.out.println(sbf.hashCode() == sbf1.hashCode());
+        System.out.println(sbf.toString().contentEquals(sbf1));
+
+    }
+}
