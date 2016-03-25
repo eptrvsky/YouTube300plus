697868c4332b8b6f472ade9738cc0898ab23ad17
svlugovoy <svlugovoy@gmail.com>
2016-03-25 20:13:37 +0200

    FibonacciNum

--- src/com/svlugovoy/youtube300plus/q160_q_q162/SortDemo.java
+++ src/com/svlugovoy/youtube300plus/q160_q_q162/SortDemo.java
@@ -76,5 +76,8 @@ public class SortDemo {
         }
     }
 
+    //insertionSort
+
+
 
 }
---
+++ src/com/svlugovoy/youtube300plus/q93_q94/SingletonDemo.java
@@ -0,0 +1,10 @@
+package com.svlugovoy.youtube300plus.q93_q94;
+
+/**
+ * @author Sergey Lugovoy <svlugovoy@gmail.com> 25.03.2016.
+ */
+public class SingletonDemo {
+
+
+
+}
